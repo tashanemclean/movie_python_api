cd123f3f0aab8f2ba157228c2b0b64ac2b0fcd28
Tashane Mclean <tashane.mclean@gmail.com>
2019-04-10 12:57:28 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from flask_heroku import Heroku
+
+app = Flask(__name__)
+CORS(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = ''
+
+heroku = Heroku(app)
+db = SQLAlchemy(app)
+
+@app.route('/')
+def home():
+    return "<h1>hello from flask</h1>"
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
